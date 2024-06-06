93d3596c4b24d34b2f25cdb98cf42b7e3dc4a05d
RafikiSmurfJr7 <rafikismurf@gmail.com>
2024-06-06 15:05:24 +0100

    Remove files from the old version

--- pages/items/error-modal.php
+++
@@ -1,9 +0,0 @@
-<div class="modal" tabindex="-1" id="errorModal">
-  <div class="modal-dialog modal-dialog-centered">
-    <div class="modal-content">
-      <div class="alert alert-<?php if(isset($_SESSION['error_alert'])){echo $_SESSION['error_alert'];unset($_SESSION['error_alert']);}?>" style="margin: 0px;">
-            <h5><i class="<?php if(isset($_SESSION['error_icon'])){echo $_SESSION['error_icon']; unset($_SESSION['error_icon']);}?>"></i>&nbsp;<?php if(isset($_SESSION['error'])){echo $_SESSION['error'];unset($_SESSION['error']);}?></h5>
-      </div>
-    </div>
-  </div>
-</div>
